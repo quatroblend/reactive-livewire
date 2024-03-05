527ab4f30271df955cd6de852508da7ffdf3833c
Sid <dev.mogyahidansid@gmail.com>
2024-03-05 17:21:18 +0800

    Installed x-intersect alpine plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+import intersect from '@alpinejs/intersect'
 
-window.Alpine = Alpine;
+Alpine.plugin(intersect)
 
-Alpine.start();
+Livewire.start()
